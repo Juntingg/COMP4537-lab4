dc248db306d5894c3d89f46500e02d3d4f7e2901
Beijzum <144724833+Beijzum@users.noreply.github.com>
2025-02-09 12:03:37 -0800

    ADD 404 test and lang

--- server/server.js
+++ server/server.js
@@ -2,9 +2,13 @@
 
 const http = require("http");
 const url = require("url");
+const msgs = require("./lang/en");
 
 http.createServer((req, res) => {
     const q = url.parse(req.url, true);
     const query = q.query;
 
+
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end(`<p style="color: red;">${msgs.error404}</p>`);
 }).listen(3000);
