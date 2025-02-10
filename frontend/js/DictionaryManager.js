ab2d4614373b9341f7f01eb2cf313f84379a8e24
Juntingg <jsu62@my.bcit.ca>
2025-02-09 16:33:27 -0800

    add DictionaryManger class

---
+++ frontend/js/DictionaryManager.js
@@ -0,0 +1,61 @@
+class DictionaryManger{
+
+    static API_BASE_URL = "http://localhost:8000/api/definitions/"
+    static async storeDefinition(word, definition, responseId){
+        if(!this.checkValidation(word) || !this.checkValidation(definition)){
+            responseId.innerHTML = INVALID_INPUT_TEXT;
+        }
+        try{
+            const xhr  = new XMLHttpRequest();
+            xhr.open("POST", this.API_BASE_URL);
+            xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+
+            xhr.onload = () =>{
+                if(xhr.status == 200){
+                    const response = JSON.parse(xhr.response);
+                    const message = response.message;
+                    if (message){
+                        responseId.innerHTML = message;
+                    }else{
+                        responseId.innerHTML = STORE_ERROR_TEXT;
+                    }
+                }else{
+                    responseId.innerHTML = STORE_ERROR_TEXT;
+                }
+            }
+            xhr.onerror = () => {
+                responseId.innerHTML = STORE_ERROR_TEXT;
+            }
+            xhr.send(JSON.stringify({ "word": word, "definition": definition }));
+        }catch(e){
+            responseId.innerHTML = `${ERROR_TEXT}: ${e}`
+        }
+    }
+
+    static async searchWord(word, responseId){
+        if (!this.checkValidation(word)) {
+            responseId.innerHTML = INVALID_INPUT_TEXT;
+        }
+        try{
+            const response = await fetch(
+                `${this.API_BASE_URL}?word=${word}`,
+                {
+                    method:"GET",
+                }
+            );
+            const data = await response.json();
+            if(!response.ok){
+                throw new Error(WORD_NOT_FOUND);
+            }
+            return data;
+
+        }catch(e){
+            responseId.innerHTML = e;
+        }
+    }
+    static checkValidation(word){
+        if (typeof input !== "string" || word.trim() === "") return false;
+        if (/\d/.test(input)) return false;
+        return true;
+    }
+}
